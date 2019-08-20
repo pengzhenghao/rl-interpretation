8d4b8b9afd76d5eb90ba0e49d0ea3baa3ed234cc
pengzh <pengzh@ie.cuhk.edu.hk>
2019-08-20 20:22:15 +0800

    completely finish this branch

--- process_fft.py
+++ process_fft.py
@@ -1,124 +1,14 @@
-import collections
-
-import gym
 import numpy as np
 import pandas
 
-from ray.rllib.env import MultiAgentEnv
-from ray.rllib.env.base_env import _DUMMY_AGENT_ID
-from ray.rllib.evaluation.episode import _flatten_action
 from ray.rllib.policy.sample_batch import DEFAULT_POLICY_ID
 from scipy.fftpack import fft
-
-from utils import DefaultMapping
-
-# %matplotlib inline
-# import seaborn as sns
-# import ray
-# ray.init(ignore_reinit_error=True, log_to_driver=)
-
-# def restore(ckpt):
-#     config = {}
-#     cls = get_agent_class('PPO')
-#     agent = cls(env="BipedalWalker-v2", config=config)
-#     agent.restore(ckpt)
-#     return agent
+import gym
+from rollout import rollout
 
 
 def default_policy_agent_mapping(unused_agent_id):
     return DEFAULT_POLICY_ID
-
-
-def rollout(agent, env_name, num_steps, seed=0, out=None, no_render=True):
-    # Mostly copied from ray.rllib.rollout
-    # But we allow the function quit when one episode is ended.
-    policy_agent_mapping = default_policy_agent_mapping
-
-    if hasattr(agent, "workers"):
-        env = agent.workers.local_worker().env
-        multiagent = isinstance(env, MultiAgentEnv)
-        if agent.workers.local_worker().multiagent:
-            policy_agent_mapping = agent.config["multiagent"
-                                                ]["policy_mapping_fn"]
-
-        policy_map = agent.workers.local_worker().policy_map
-        state_init = {p: m.get_initial_state() for p, m in policy_map.items()}
-        use_lstm = {p: len(s) > 0 for p, s in state_init.items()}
-        action_init = {
-            p: m.action_space.sample()
-            for p, m in policy_map.items()
-        }
-    else:
-        env = gym.make(env_name)
-        multiagent = False
-        use_lstm = {DEFAULT_POLICY_ID: False}
-
-    env.seed(seed)
-    if out is not None:
-        rollouts = []
-    steps = 0
-    #     while steps < (num_steps or steps + 1):
-    mapping_cache = {}  # in case policy_agent_mapping is stochastic
-    if out is not None:
-        rollout = []
-    obs = env.reset()
-    agent_states = DefaultMapping(
-        lambda agent_id: state_init[mapping_cache[agent_id]]
-    )
-    prev_actions = DefaultMapping(
-        lambda agent_id: action_init[mapping_cache[agent_id]]
-    )
-    prev_rewards = collections.defaultdict(lambda: 0.)
-    done = False
-    reward_total = 0.0
-    rollout = []
-    while not done and steps < (num_steps or steps + 1):
-        multi_obs = obs if multiagent else {_DUMMY_AGENT_ID: obs}
-        action_dict = {}
-        for agent_id, a_obs in multi_obs.items():
-            if a_obs is not None:
-                policy_id = mapping_cache.setdefault(
-                    agent_id, policy_agent_mapping(agent_id)
-                )
-                p_use_lstm = use_lstm[policy_id]
-                if p_use_lstm:
-                    a_action, p_state, _ = agent.compute_action(
-                        a_obs,
-                        state=agent_states[agent_id],
-                        prev_action=prev_actions[agent_id],
-                        prev_reward=prev_rewards[agent_id],
-                        policy_id=policy_id
-                    )
-                    agent_states[agent_id] = p_state
-                else:
-                    a_action = agent.compute_action(
-                        a_obs,
-                        prev_action=prev_actions[agent_id],
-                        prev_reward=prev_rewards[agent_id],
-                        policy_id=policy_id
-                    )
-                a_action = _flatten_action(a_action)  # tuple actions
-                action_dict[agent_id] = a_action
-                prev_actions[agent_id] = a_action
-        action = action_dict
-
-        action = action if multiagent else action[_DUMMY_AGENT_ID]
-        next_obs, reward, done, _ = env.step(action)
-        if multiagent:
-            for agent_id, r in reward.items():
-                prev_rewards[agent_id] = r
-        else:
-            prev_rewards[_DUMMY_AGENT_ID] = reward
-
-        if multiagent:
-            done = done["__all__"]
-            reward_total += sum(reward.values())
-        else:
-            reward_total += reward
-        rollout.append([obs, action, next_obs, reward, done])
-        steps += 1
-        obs = next_obs
-    return rollout
 
 
 def compute_fft(y, normalize=True):
@@ -172,7 +62,10 @@ def stack_fft(obs, act, use_log=True, normalize=True):
 
 
 def rollout_once(agent, seed=0):
-    ret = rollout(agent, "BipedalWalker-v2", 0, seed)
+    env = gym.make("BipedalWalker-v2")
+    env.seed(seed)
+    ret = rollout(agent, env, require_trajectory=True)
+    ret = ret["trajectory"]
     obs = np.array([a[0] for a in ret])
     act = np.array([a[1] for a in ret])
     return obs, act
