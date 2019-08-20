ecc89c5f029cce92456b836d0f14fed6596895e8
pengzh <pengzh@ie.cuhk.edu.hk>
2019-08-20 20:46:08 +0800

    implement fft worker

--- process_fft.py
+++ process_fft.py
@@ -5,11 +5,10 @@ from ray.rllib.policy.sample_batch import DEFAULT_POLICY_ID
 from scipy.fftpack import fft
 import gym
 from rollout import rollout
-
+import ray
 
 def default_policy_agent_mapping(unused_agent_id):
     return DEFAULT_POLICY_ID
-
 
 def compute_fft(y, normalize=True):
     y = np.asarray(y)
@@ -25,8 +24,7 @@ def compute_fft(y, normalize=True):
     yf2 = yf1[:int(len(y) / 2)]  # 由于对称性，只取一半区间
     return yf2
 
-
-def stack_fft(obs, act, use_log=True, normalize=True):
+def stack_fft(obs, act, normalize, use_log=True):
     obs = np.asarray(obs)
     act = np.asarray(act)
 
@@ -60,58 +58,67 @@ def stack_fft(obs, act, use_log=True, normalize=True):
 
     return pandas.DataFrame(result)
 
+@ray.remote
+class FFTWorker(object):
 
-def rollout_once(agent, seed=0):
-    env = gym.make("BipedalWalker-v2")
-    env.seed(seed)
-    ret = rollout(agent, env, require_trajectory=True)
-    ret = ret["trajectory"]
-    obs = np.array([a[0] for a in ret])
-    act = np.array([a[1] for a in ret])
-    return obs, act
+    def __init__(self, ckpt):
+        # restore an agent here
+        self.agent = None
+        self.env_maker = None
+        self.agent_name = None
+        pass
 
+    def _rollout(self, env):
+        ret = rollout(self.agent, env, require_trajectory=True)
+        ret = ret["trajectory"]
+        obs = np.array([a[0] for a in ret])
+        act = np.array([a[1] for a in ret])
+        return obs, act
 
-def rollout_multiple(
-        agent_name, agent, seed_list, normalize=True, stack_rollout=True
-):
-    assert isinstance(seed_list, list)
-    data_frame = None
-    obs_list = []
-    act_list = []
-    for seed in seed_list:
-        print("Current Testing Agent: <{}>, Seed: {}".format(agent_name, seed))
-        obs, act = rollout_once(agent, seed)
-        if not stack_rollout:
-            df = stack_fft(obs, act, normalize=normalize)
-            df.insert(df.shape[1], "seed", seed)
-            data_frame = df if data_frame is None else \
-                data_frame.append(df, ignore_index=True)
-        else:
-            obs_list.append(obs)
-            act_list.append(act)
-    if stack_rollout:
-        data_frame = stack_fft(
-            np.concatenate(obs_list),
-            np.concatenate(act_list),
-            normalize=normalize
-        )
-    data_frame.insert(data_frame.shape[1], "agent", agent_name)
-    return data_frame
+    def _rollout_multiple(self, number_of_rollouts, seed,
+                          stack=False, normalize=True):
+        # One seed, N rollouts.
+        env = self.env_maker()
+        env.seed(seed)
+        data_frame = None
+        obs_list = []
+        act_list = []
+        for i in range(number_of_rollouts):
+            print("Agent {}, Rollout {}/{}".format(self.agent_name, i, number_of_rollouts))
+            obs, act = self._rollout(env)
+            if not stack:
+                df = stack_fft(obs, act, normalize=normalize)
+                df.insert(df.shape[1], "seed", seed)
+                data_frame = df if data_frame is None else \
+                    data_frame.append(df, ignore_index=True)
+            else:
+                obs_list.append(obs)
+                act_list.append(act)
+        if stack:
+            data_frame = stack_fft(
+                np.concatenate(obs_list),
+                np.concatenate(act_list),
+                normalize=normalize
+            )
+        data_frame.insert(data_frame.shape[1], "agent", self.agent_name)
+        return data_frame
 
+    def _get_representation(self, data_frame):
+        representation = None
+        # TODO
+        return representation
 
-def evaluate_agents_frequency_character(
-        agent_dict, seed_list, normalize=True, stack_rollout=False
-):
-    data_frame = None
-    for agent_name, agent in agent_dict.items():
-        df = rollout_multiple(
-            agent_name, agent, seed_list, normalize, stack_rollout
-        )
-        if data_frame is None:
-            data_frame = df
-        else:
-            data_frame = data_frame.append(df, ignore_index=True)
-    return data_frame
-
-
-evaluate_different_agents = evaluate_agents_frequency_character
+    @ray.method(num_return_vals=1)
+    def fft(self, num_seeds, num_rollouts, stack=False, clip=None, normalize=True,
+            log=True):
+        data_frame = None
+        for seed in range(num_seeds):
+            df = self._rollout_multiple(num_rollouts, seed, stack, normalize)
+            if data_frame is None:
+                data_frame = df
+            else:
+                data_frame = data_frame.append(df, ignore_index=True)
+        if clip:
+            pass
+            # TODO
+        return self._get_representation(data_frame)
