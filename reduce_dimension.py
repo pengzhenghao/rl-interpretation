226ccf6f8c7fb4bbc44a0f7bebe9e2c170c3a515
pengzh <pengzh@ie.cuhk.edu.hk>
2019-08-24 15:23:13 +0800

    Finish the visualization module

--- reduce_dimension.py
+++ reduce_dimension.py
@@ -4,89 +4,37 @@ This code is copied from uber atari-model-zoo projects. Link:
 https://github.com/uber-research/atari-model-zoo/blob/master/
 dimensionality_reduction/process_helper.py
 """
-
 import matplotlib.pyplot as plt
 import pandas
 import seaborn as sns
-# from collections import OrderedDict
 from sklearn import decomposition, manifold
-
-# import atari_zoo
-# from atari_zoo import MakeAtariModel
-# import urllib.request
-# import os
-# method= {
-#     "name": "pca_tsne",
-#     "pca_dim": 50,
-#     "perplexity": 30,
-#     "n_iter": 3000
-# }
 
 DEFAULT_METHOD = {
     "name": "pca_tsne",
     "pca_dim": 50,
     "perplexity": 30,
-    "n_iter": 1000,
-    "tsne_dim": 2
+    "n_iter": 3000
 }
 
 
-def reduce_dimension(data, prediction, three_dimensional):
-    # print(method)
-
-    print("Reducing Dimensions")
+def reduce_dimension(data, prediction, three_dimensional=False):
     method = DEFAULT_METHOD
     perplexity = method['perplexity']
     n_iter = method['n_iter']
     pca_dim = method['pca_dim']
     tsne_dim = 3 if three_dimensional else 2
-    # if 'tsne' in method['name']:
-    #     if 'perplexity' in method:
-    #         perplexity = method['perplexity']
-    #     if 'n_iter' in method:
-    #         n_iter = method['n_iter']
-    #     if 'pca_dim' in method:
-    #         pca_dim = method['pca_dim']
-    #     if 'tsne_dim' in method:
-    #         tsne_dim = method['tsne_dim']
 
-    # print(perplexity, n_iter, pca_dim)
-    # if method['name'] == 'tsne':
-    #     print('running tsne')
-    #     result = manifold.TSNE(
-    #         n_components=2,
-    #         perplexity=perplexity,
-    #         verbose=2,
-    #         random_state=0,
-    #         n_iter=n_iter
-    #     ).fit_transform(data)
-    # elif method['name'] == 'pca':
-    #     print('running pca')
-    #     result = decomposition.PCA(2).fit_transform(data)
-    if method['name'] == 'pca_tsne':
-        print('running pca')
-        pca_result = decomposition.PCA(pca_dim).fit_transform(data)
-        print('running tsne')
-        result = manifold.TSNE(
-            n_components=tsne_dim,
-            perplexity=perplexity,
-            verbose=2,
-            random_state=0,
-            n_iter=n_iter
-        ).fit_transform(pca_result)
-    # elif method['name'] == 'debug':
-    #     print('running debug')
-    #     nrow, ncol = data.shape
-    #     idx_last_x, idx_last_y = int(ncol / 2 - 1), -1
-    #     result = np.hstack(
-    #         (
-    #             data[:, idx_last_x].reshape(nrow, 1),
-    #             data[:, idx_last_y].reshape(nrow, 1)
-    #         )
-    #     )
-    else:
-        raise NotImplementedError
+    print('Running pca')
+    pca_result = decomposition.PCA(pca_dim).fit_transform(data)
 
+    print('Running tsne')
+    result = manifold.TSNE(
+        n_components=tsne_dim,
+        perplexity=perplexity,
+        verbose=2,
+        random_state=0,
+        n_iter=n_iter
+    ).fit_transform(pca_result)
     print(
         'Reduction Completed! data.shape={} result.shape={}'.format(
             data.shape, result.shape
@@ -94,33 +42,26 @@ def reduce_dimension(data, prediction, three_dimensional):
     )
 
     def get_row(name, coordinates):
+        ret = {
+            "agent": name,
+            "x": coordinates[0],
+            "y": coordinates[1],
+            "cluster": prediction[name]['cluster']
+        }
         if three_dimensional:
-            ret = {
-                "agent": name,
-                "x": coordinates[0],
-                "y": coordinates[1],
-                "z": coordinates[2],
-                "cluster": prediction[name]['cluster']
-            }
-        else:
-            ret = {
-                "agent": name,
-                "x": coordinates[0],
-                "y": coordinates[1],
-                "cluster": prediction[name]['cluster']
-            }
+            ret['z'] = coordinates[2]
         return ret
 
     result_df = pandas.DataFrame(
         [
             get_row(name, coord)
-            for name, coord in zip(prediction.index, result)
+            for name, coord in zip(prediction.keys(), result)
         ]
     )
     return result_df, result
 
 
-def display(plot_df):
+def draw(plot_df):
     three_dimensional = 'z' in plot_df.columns
     if three_dimensional:
         _draw_3d(plot_df)
@@ -146,7 +87,7 @@ def _draw_2d(plot_df):
 
 def _draw_3d(plot_df):
     from mpl_toolkits.mplot3d import Axes3D
-    Axes3D()
+    use_less_var = Axes3D
 
     fig = plt.figure(figsize=(12, 10), dpi=300)
     ax = fig.add_subplot(111, projection='3d')
@@ -165,20 +106,12 @@ def _draw_3d(plot_df):
     ax.legend()
 
     ax.set_title(_get_title(plot_df))
+    plt.show()
 
 
 def _get_title(plot_df):
     num_clusters = len(plot_df.cluster.unique())
     num_agents = len(plot_df.agent.unique())
     return "Clustering Result of {} Clusters, " \
-           "{} Agents (Dimensions Reduced by PCA-tSNE)".format(
+           "{} Agents (Dimensions Reduced by PCA-TSNE)".format(
         num_clusters, num_agents)
-
-
-def test():
-    from process_cluster import load_cluster_df
-    cluster_df = load_cluster_df("")
-
-
-if __name__ == '__main__':
-    test()
