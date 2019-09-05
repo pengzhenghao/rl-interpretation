5af801da834e121c6d3f325e82e846d934be85e7
pengzh <pengzh@ie.cuhk.edu.hk>
2019-09-05 11:07:52 +0800

    Good Morning!

--- tmp0904.sh
+++ tmp0904.sh
@@ -1,5 +1,2 @@
 export PYTHONUNBUFFERED=1
-python rollout.py --yaml-path data/ppo-300-agents.yaml --force-rewrite --num-workers 16
-wait
-python rollout.py --yaml-path data/test_ablation_result.yaml --force-rewrite --num-workers 16
-wait
+python rollout.py --yaml-path yaml/ppo-20-agents-0903-progress.yaml --force-rewrite --num-workers 16
