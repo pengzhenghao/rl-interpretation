86b332f9cb6a6e502b2169cb114997b2083c9307
pengzh <pengzh@ie.cuhk.edu.hk>
2019-09-19 11:53:25 +0800

    add Spearman and Pearson correlation coefficient modules

--- toolbox/correlate/pearson.py
+++ toolbox/correlate/pearson.py
@@ -1,11 +1,16 @@
-import numpy as np
-import scipy
 import time
 
+import numpy as np
+import scipy
 
-def get_pearson_correlation_coefficient(probe_mask, new_obs):
+from toolbox.visualize.draw_2d import draw_heatmap
+
+
+def get_pearson(probe_mask, new_obs,
+                batch_size=1000000, pval_th=0.05):
     sample_size = probe_mask.shape[0]
     assert sample_size == new_obs.shape[0]
+    assert sample_size >= batch_size
     mask_length = probe_mask.shape[1]
     obs_length = new_obs.shape[1]
 
@@ -16,12 +21,31 @@ def get_pearson_correlation_coefficient(probe_mask, new_obs):
 
     for mask_id in range(mask_length):
         print("[T +{:.2f}s/{:.2f}s]Current working row: {}".format(
-            time.time()-now, time.time()-start, mask_id)
+            time.time() - now, time.time() - start, mask_id)
         )
         now = time.time()
         for obs_id in range(obs_length):
-            rho, pval = scipy.stats.pearsonr(probe_mask[:, mask_id],
-                                             new_obs[:, obs_id])
+            rho, pval = scipy.stats.pearsonr(
+                probe_mask[:batch_size, mask_id],
+                new_obs[:batch_size, obs_id]
+            )
             rho_matrix[mask_id, obs_id] = rho
             p_matrix[mask_id, obs_id] = pval
-    return rho_matrix, p_matrix
+
+    print_mask = p_matrix < pval_th
+    print(
+        "We found {} of pairs that have 95% confidence. The proportion is"
+        "{}.".format(
+            print_mask.sum(),
+            print_mask.sum() / (print_mask.shape[0] * print_mask.shape[1]))
+    )
+
+    return rho_matrix, p_matrix, print_mask
+
+
+# get_pearson = get_pearson_correlation_coefficient
+
+
+def draw_pearson(rho, pval, mask):
+    rho = rho * mask
+    draw_heatmap(rho, matrix2=pval)
---
+++ toolbox/correlate/spearman.py
@@ -0,0 +1,28 @@
+import scipy
+
+from toolbox.visualize.draw_2d import draw_heatmap
+
+
+def get_spearman(probe_mask, norm_next_obs_diff, batch_size=1000000,
+                 pval_th=0.05):
+    rho, pval = scipy.stats.spearmanr(probe_mask[:batch_size],
+                                      norm_next_obs_diff[:batch_size], axis=0)
+
+    mask = pval < pval_th  # small p_val means greater confidence.
+
+    print_mask = mask[:probe_mask.shape[1], probe_mask.shape[1]:]
+
+    print(
+        "We found {} of pairs that have 95% confidence. The proportion is"
+        "{}.".format(
+            print_mask.sum(),
+            print_mask.sum() / (print_mask.shape[0] * print_mask.shape[1]))
+    )
+
+    return rho, pval, mask
+
+
+def draw_spearman(rho, pval, mask, size):
+    rho = rho * mask
+    # size = mask.shape[1]
+    draw_heatmap(rho[:size, size:], matrix2=pval[:size, size:])
