3a3928b14243f587b6a70dcc9267c09c66a214f7
pengzh <pengzh@ie.cuhk.edu.hk>
2019-10-22 13:10:47 +0800

    finish finetune worker and finetune manager

--- toolbox/train/finetune.py
+++ toolbox/train/finetune.py
@@ -1,37 +1,35 @@
-from toolbox.utils import initialize_ray, has_gpu
-from collections import OrderedDict
-import time
-import ray
 import copy
+import time
+from collections import OrderedDict
+
+import ray
+
+from toolbox.evaluate.symbolic_agent import SymbolicAgentBase
+from toolbox.utils import has_gpu
 
 
-class _RemoteSymbolicReplayWorker:
+class _RemoteSymbolicTrainWorker:
     @classmethod
     def as_remote(cls, num_cpus=None, num_gpus=None, resources=None):
         return ray.remote(
             num_cpus=num_cpus, num_gpus=num_gpus, resources=resources
         )(cls)
 
-    def __init__(self):
-        pass
-
     def finetune(self, symbolic_agent, num_iters):
-        pass
-        # if self.existing_agent is None:
-        #     agent = symbolic_agent.get()['agent']
-        #     self.existing_agent = agent
-        # else:
-        #     agent = symbolic_agent.get(self.existing_agent)['agent']
-        # ret = agent_replay(agent, obs)
-        # return ret
+        agent = symbolic_agent.get()
+        result_list = []
+        for i in range(num_iters):
+            result = agent.train()
+            result_list.append(result)
+        return result
 
 
-class RemoteSymbolicReplayManager:
+class RemoteSymbolicTrainManager:
     def __init__(self, num_workers, total_num=None, log_interval=50):
         self.num_workers = num_workers
         num_gpus = 3.8 / num_workers if has_gpu() else 0
         self.workers = [
-            _RemoteSymbolicReplayWorker.as_remote(num_gpus=num_gpus).remote()
+            _RemoteSymbolicTrainWorker.as_remote(num_gpus=num_gpus).remote()
             for _ in range(num_workers)
         ]
         self.pointer = 0
@@ -43,9 +41,10 @@ class RemoteSymbolicReplayManager:
         self.total_num = total_num
         self.log_interval = log_interval
 
-    def replay(self, index, symbolic_agent, obs):
-        # assert isinstance(symbolic_agent, SymbolicAgentBase)
-        oid = self.workers[self.pointer].replay.remote(symbolic_agent, obs)
+    def train(self, index, symbolic_agent, num_iters):
+        assert isinstance(symbolic_agent, SymbolicAgentBase)
+        oid = self.workers[self.pointer].finetune.remote(symbolic_agent,
+                                                         num_iters)
         self.obj_dict[index] = oid
         self.pointer += 1
         if self.pointer == self.num_workers:
@@ -60,7 +59,7 @@ class RemoteSymbolicReplayManager:
             self.finish_count += 1
             if self.finish_count % self.log_interval == 0:
                 print(
-                    "[{}/{}] (+{:.2f}s/{:.2f}s) Finish replay: {}!".format(
+                    "[{}/{}] (+{:.2f}s/{:.2f}s) Finish train: {}!".format(
                         self.finish_count, self.total_num,
                         time.time() - self.now,
                         time.time() - self.start, name
@@ -72,7 +71,3 @@ class RemoteSymbolicReplayManager:
     def get_result(self):
         self._collect()
         return self.ret_dict
-
-
-class FinetuneWorker:
-    pass
