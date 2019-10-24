e1d2f67793cc1c4f3cb457841084c90c41b46c17
pengzh <pengzh@ie.cuhk.edu.hk>
2019-10-24 15:39:15 +0800

    now we support change the std

--- toolbox/visualize/google_chart/google_chart.js
+++ toolbox/visualize/google_chart/google_chart.js
@@ -11,16 +11,31 @@ var rawData = $.parseJSON($.ajax({
 
 
 function drawChart() {
-    data_table = new google.visualization.DataTable(rawData['data']);
 
-    // Fill the tooltip
-    data_table.addColumn(
-        {
-            'type': 'string',
-            'role': 'tooltip',
-            'p': {'html': true, 'role': "tooltip"}
-        }
-    );
+    var info = rawData['info']
+
+    // Create the slider for std changing
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");
+    slider.value = info['std_min'];
+    slider.min = info['std_min'];
+    slider.max = info['num_std'] - 1;
+    output.innerHTML = slider.value;
+
+    slider.oninput = function () {
+        var std = get_exact_std(this.value)
+        output.innerHTML = std;
+        change_data(std);
+    };
+
+    function get_exact_std(slider_value) {
+        return slider_value / (info['num_std'] - 1)
+    }
+
+    function change_data(std) {
+        data_table = get_data_table(std);
+        chart.draw(data_table, options);
+    }
 
     function createCustomHTMLContent(hyperLink, imagePath, agentName) {
         return '<dev style="padding:5px 5px 5px 5px;"><a href="' + hyperLink + '"><img src=\"' +
@@ -29,19 +44,34 @@ function drawChart() {
             '" style=\"width:100px; height:100px\"  ></a></dev>';
     }
 
-    var url, link, cell_html, row;
-    for (row = 0; row < data_table.getNumberOfRows(); row++) {
-        url = data_table.getRowProperty(row, "url");
-        link = data_table.getRowProperty(row, "link");
-        cell_html = createCustomHTMLContent(link, url, row);
-        // console.log(cell_html);
-        data_table.setCell(row, 2, cell_html);
+    function get_data_table(std) {
+        var data_table = new google.visualization.DataTable(rawData['data'][std]);
+
+        // Fill the tooltip
+        data_table.addColumn(
+            {
+                'type': 'string',
+                'role': 'tooltip',
+                'p': {'html': true, 'role': "tooltip"}
+            }
+        );
+
+        var url, link, cell_html, row;
+        for (row = 0; row < data_table.getNumberOfRows(); row++) {
+            url = data_table.getRowProperty(row, "url");
+            link = data_table.getRowProperty(row, "link");
+            cell_html = createCustomHTMLContent(link, url, row);
+            data_table.setCell(row, 2, cell_html);
+        }
+        return data_table
     }
+
+    // Init the chart first.
+    var init_std = "0";
+    data_table = get_data_table(init_std);
 
     // Create the chart
     var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
-
-    var info = rawData['info']
     var options = {
         tooltip: {isHtml: true, trigger: 'selection'},
         title: info['title'],
@@ -61,24 +91,5 @@ function drawChart() {
     };
 
     chart.draw(data_table, options);
-
-    // Create the slider for std changing
-    var slider = document.getElementById("myRange");
-    var output = document.getElementById("demo");
-    output.innerHTML = slider.value;
-
-    slider.oninput = function () {
-        var std = get_exact_std(this.value)
-        output.innerHTML = std;
-        change_data(std);
-    };
-
-    function get_exact_std(slider_value) {
-        return slider_value * 1.5
-    }
-
-    function change_data(std) {
-        return null
-    }
 
 }
