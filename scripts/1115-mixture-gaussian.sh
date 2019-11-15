00b34bb54579fe2be912fef95f12f6412bb76e52
pengzh <pengzh@ie.cuhk.edu.hk>
2019-11-15 12:36:46 +0800

    training script for today

---
+++ scripts/1115-mixture-gaussian.sh
@@ -0,0 +1,23 @@
+# Please call this script at novel-rl directory.
+
+exp_name="DELETE_ME_TEST_1115-mixture_gaussian"
+
+env_name="BipedalWalker-v2"
+nohup python toolbox/action_distribution/train_mixture_gaussian.py \
+--exp-name ${exp_name}_${env_name} --num-gpus 1 --env ${env_name} \
+> ${exp_name}_${env_name}.log 2&1 &
+
+env_name="HalfCheetah-v2"
+nohup python toolbox/action_distribution/train_mixture_gaussian.py \
+--exp-name ${exp_name}_${env_name} --num-gpus 1 --env ${env_name} \
+> ${exp_name}_${env_name}.log 2&1 &
+
+env_name="Walker2d-v3"
+nohup python toolbox/action_distribution/train_mixture_gaussian.py \
+--exp-name ${exp_name}_${env_name} --num-gpus 1 --env ${env_name} \
+> ${exp_name}_${env_name}.log 2&1 &
+
+env_name="Hopper-v2"
+nohup python toolbox/action_distribution/train_mixture_gaussian.py \
+--exp-name ${exp_name}_${env_name} --num-gpus 1 --env ${env_name} \
+> ${exp_name}_${env_name}.log 2&1 &
