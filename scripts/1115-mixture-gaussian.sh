068761d5551ada9191e079364ee9e62157bec832
pengzh <pengzh@ie.cuhk.edu.hk>
2019-11-15 18:15:14 +0800

    we meet resource temporary unavailable error

--- scripts/1115-mixture-gaussian.sh
+++ scripts/1115-mixture-gaussian.sh
@@ -2,12 +2,6 @@
 
 exp_name="1115-mixture_gaussian"
 
-env_name="HalfCheetah-v2"
-nohup python \
-toolbox/action_distribution/train_mixture_gaussian.py \
---exp-name ${exp_name}_${env_name} --num-gpus 5 --env ${env_name} \
---num-seeds 1 > ${exp_name}_${env_name}.log 2>&1 &
-
 env_name="BipedalWalker-v2"
 CUDA_VISIBLE_DEVICES=1 nohup python \
 toolbox/action_distribution/train_mixture_gaussian.py \
@@ -24,4 +18,10 @@ env_name="Hopper-v2"
 CUDA_VISIBLE_DEVICES=3 nohup python \
 toolbox/action_distribution/train_mixture_gaussian.py \
 --exp-name ${exp_name}_${env_name} --num-gpus 1 --env ${env_name} \
+--num-seeds 1 > ${exp_name}_${env_name}.log 2>&1 &
+
+env_name="HalfCheetah-v2"
+OMP_NUM_THREADS=1 nohup python \
+toolbox/action_distribution/train_mixture_gaussian.py \
+--exp-name ${exp_name}_${env_name} --num-gpus 5 --env ${env_name} \
 --num-seeds 1 > ${exp_name}_${env_name}.log 2>&1 &
