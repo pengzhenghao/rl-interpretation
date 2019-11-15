c99dd3c16acf0a1d8b50e2a743be7a9f4e81e176
pengzh <pengzh@ie.cuhk.edu.hk>
2019-11-15 17:10:12 +0800

    prepare to run bug-free exps

--- scripts/1115-mixture-gaussian.sh
+++ scripts/1115-mixture-gaussian.sh
@@ -5,19 +5,19 @@ exp_name="1115-mixture_gaussian"
 env_name="BipedalWalker-v2"
 nohup python toolbox/action_distribution/train_mixture_gaussian.py \
 --exp-name ${exp_name}_${env_name} --num-gpus 1 --env ${env_name} \
-> ${exp_name}_${env_name}.log 2>&1 &
+--num-seeds 1 > ${exp_name}_${env_name}.log 2>&1 &
 
 env_name="HalfCheetah-v2"
 nohup python toolbox/action_distribution/train_mixture_gaussian.py \
 --exp-name ${exp_name}_${env_name} --num-gpus 1 --env ${env_name} \
-> ${exp_name}_${env_name}.log 2>&1 &
+--num-seeds 1 > ${exp_name}_${env_name}.log 2>&1 &
 
 env_name="Walker2d-v3"
 nohup python toolbox/action_distribution/train_mixture_gaussian.py \
 --exp-name ${exp_name}_${env_name} --num-gpus 1 --env ${env_name} \
-> ${exp_name}_${env_name}.log 2>&1 &
+--num-seeds 1 > ${exp_name}_${env_name}.log 2>&1 &
 
 env_name="Hopper-v2"
 nohup python toolbox/action_distribution/train_mixture_gaussian.py \
 --exp-name ${exp_name}_${env_name} --num-gpus 1 --env ${env_name} \
-> ${exp_name}_${env_name}.log 2>&1 &
+--num-seeds 1 > ${exp_name}_${env_name}.log 2>&1 &
