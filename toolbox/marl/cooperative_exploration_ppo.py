fdf4a000f3e3d9b139433685b4adf64dd4f8a724
pengzh <pengzh@ie.cuhk.edu.hk>
2019-12-02 00:01:48 +0800

    "Use myself vf_pred" version of CEPPO

--- toolbox/marl/cooperative_exploration_ppo.py
+++ toolbox/marl/cooperative_exploration_ppo.py
@@ -1,15 +1,14 @@
 import logging
 
-import numpy as np
 import tensorflow as tf
 from ray.rllib.agents.ppo.ppo import PPOTrainer, DEFAULT_CONFIG
 from ray.rllib.agents.ppo.ppo_policy import Postprocessing, SampleBatch, \
     BEHAVIOUR_LOGITS, ACTION_LOGP, postprocess_ppo_gae, PPOTFPolicy, \
-    PPOLoss as OriginalPPOLoss, kl_and_loss_stats
+    PPOLoss as OriginalPPOLoss, setup_mixins, make_tf_callable
 
 from toolbox.marl.extra_loss_ppo_trainer import merge_dicts, \
-    LocalMultiGPUOptimizerModified, chop_into_sequences, MultiAgentBatch, \
-    SyncSamplesOptimizer, mixin_list, setup_mixins, validate_config
+    LocalMultiGPUOptimizerModified, chop_into_sequences, \
+    SyncSamplesOptimizer, validate_config, mixin_list
 
 logger = logging.getLogger(__name__)
 
@@ -45,18 +44,11 @@ def ceppo_loss(policy, model, dist_class, train_batch):
     logits, state = model.from_batch(train_batch)
     action_dist = dist_class(logits, model)
 
-    if state:
-        max_seq_len = tf.reduce_max(train_batch["seq_lens"])
-        mask = tf.sequence_mask(train_batch["seq_lens"], max_seq_len)
-        mask = tf.reshape(mask, [-1])
-    else:
-        mask = tf.ones_like(
-            train_batch[Postprocessing.ADVANTAGES], dtype=tf.bool)
-
     other_names = set(
         k.split('-')[1] for k in train_batch.keys() if k.startswith("peer"))
     assert other_names or policy.config['disable']
     other_names.add("prev")
+
     policy.loss_dict = {}
     my_name = tf.get_variable_scope().name
     for peer in other_names:
@@ -65,6 +57,16 @@ def ceppo_loss(policy, model, dist_class, train_batch):
         if policy.config['disable'] and peer != "prev":
             continue
         find = lambda x: x if peer == "prev" else "peer-{}-{}".format(peer, x)
+
+        if state:
+            raise NotImplementedError()
+            # max_seq_len = tf.reduce_max(train_batch["seq_lens"])
+            # mask = tf.sequence_mask(train_batch["seq_lens"], max_seq_len)
+            # mask = tf.reshape(mask, [-1])
+        else:
+            mask = tf.ones_like(train_batch[find(Postprocessing.ADVANTAGES)],
+                                dtype=tf.bool)
+
         policy.loss_dict[peer] = OriginalPPOLoss(
             policy.action_space,
             dist_class,
@@ -94,107 +96,92 @@ def ceppo_loss(policy, model, dist_class, train_batch):
     return policy.loss_obj.loss
 
 
-class AddLossMixin(object):
-    """When training, this Mixin add the sharing data into feed_dict.
 
-    Originally, it's a function of tf_policy, so we copied some codes from
-    tf_policy.py"""
-
-    def _get_loss_inputs_dict(
-            self, batch, shuffle, cross_policy_obj, policy_id=None
-    ):
-        """When training, add the required data into the feed_dict."""
-        feed_dict = {}
-        for pid, batch in cross_policy_obj.items():
-            if pid == policy_id or self.config['disable']:
-                continue
-            for k in required_peer_data_keys:
-                item_name = "peer-{}-{}".format(pid, k)
-                feed_dict[self._loss_input_dict[item_name]] = batch[k]
-
-        """The below codes are copied from rllib. """
-        if self._batch_divisibility_req > 1:
-            meets_divisibility_reqs = (
-                    len(batch[SampleBatch.CUR_OBS]) %
-                    self._batch_divisibility_req == 0
-                    and max(batch[SampleBatch.AGENT_INDEX]) == 0
-            )  # not multiagent
-        else:
-            meets_divisibility_reqs = True
-        # Simple case: not RNN nor do we need to pad
-        if not self._state_inputs and meets_divisibility_reqs:
-            if shuffle:
-                batch.shuffle()
-            for k, ph in self._loss_inputs:
-                if k in batch:
-                    feed_dict[ph] = batch[k]
-            return feed_dict
-        if self._state_inputs:
-            max_seq_len = self._max_seq_len
-            dynamic_max = True
-        else:
-            max_seq_len = self._batch_divisibility_req
-            dynamic_max = False
-        # RNN or multi-agent case
-        feature_keys = [k for k, v in self._loss_inputs]
-        state_keys = [
-            "state_in_{}".format(i) for i in range(len(self._state_inputs))
-        ]
-        feature_sequences, initial_states, seq_lens = chop_into_sequences(
-            batch[SampleBatch.EPS_ID],
-            batch[SampleBatch.UNROLL_ID],
-            batch[SampleBatch.AGENT_INDEX], [batch[k] for k in feature_keys],
-            [batch[k] for k in state_keys],
-            max_seq_len,
-            dynamic_max=dynamic_max,
-            shuffle=shuffle
-        )
-        for k, v in zip(feature_keys, feature_sequences):
-            feed_dict[self._loss_input_dict[k]] = v
-        for k, v in zip(state_keys, initial_states):
-            feed_dict[self._loss_input_dict[k]] = v
-        feed_dict[self._seq_lens] = seq_lens
-
-        assert set(feed_dict.keys()) == set(self._loss_input_dict.keys()), \
-            (set(feed_dict.keys()), set(self._loss_input_dict.keys()))
-        return feed_dict
-
-
-def postprocess_ceppo(policy, sample_batch, other_agent_batches=None,
-                      epidose=None):
-    batch = postprocess_ppo_gae(policy, sample_batch,
-                                other_agent_batches, epidose)
+def postprocess_ceppo(policy, sample_batch, others_batches=None, epidose=None):
     if not policy.loss_initialized():
-        for key in required_peer_data_keys:
-            for name in policy.config["multiagent"]["policies"].keys():
-                assert not key.startswith("peer")
-                batch["peer-{}-{}".format(name, key)] = \
-                    np.zeros_like(batch[key], dtype=np.float32)
+        # Only for initialization. GAE postprocess done in the following func.
+        batch = postprocess_ppo_gae(policy, sample_batch,
+                                    others_batches, epidose)
+        # for key in required_peer_data_keys:
+        #     for name in policy.config["multiagent"]["policies"].keys():
+        #         assert not key.startswith("peer")
+        #         batch["peer-{}-{}".format(name, key)] = \
+        #             np.zeros_like(batch[key], dtype=np.float32)
+    else:
+        if policy.config['disable']:
+            batch = sample_batch
+        else:
+            # Fuse
+            batch = SampleBatch.concat_samples(
+                [sample_batch] +
+                [b for (_, b) in others_batches.values()]
+            )
+
+            # Replay to collect vf_pred
+            policy.model.from_batch(batch, False)
+            @make_tf_callable(policy.get_session())
+            def value_batch(ob, prev_action, prev_reward):
+                # We do not support recurrent network now.
+                model_out, _ = policy.model({
+                    SampleBatch.CUR_OBS: tf.convert_to_tensor(ob),
+                    SampleBatch.PREV_ACTIONS: tf.convert_to_tensor(
+                        prev_action),
+                    SampleBatch.PREV_REWARDS: tf.convert_to_tensor(
+                        prev_reward),
+                    "is_training": tf.convert_to_tensor(False),
+                })
+                return policy.model.value_function()
+
+            batch[SampleBatch.VF_PREDS] = value_batch(
+                batch[SampleBatch.CUR_OBS],
+                batch[SampleBatch.ACTIONS],
+                batch[SampleBatch.REWARDS]
+            )
+
+        # Collect advantages
+        batch = postprocess_ppo_gae(policy, batch)
     return batch
 
 
-def get_cross_policy_object(multi_agent_batch, self_optimizer):
-    """Add contents into cross_policy_object, which passed to each policy."""
-    count_dict = {k: v.count for k, v in
-                  multi_agent_batch.policy_batches.items()}
-    for k in self_optimizer.workers.local_worker().policy_map.keys():
-        if k not in count_dict:
-            count_dict[k] = 0
-    sample_size = max(count_dict.values())
-    ret = {}
-    if min(count_dict.values()) < sample_size:
-        samples = [multi_agent_batch]
-        while min(count_dict.values()) < sample_size:
-            tmp_batch = self_optimizer.workers.local_worker().sample()
-            samples.append(tmp_batch)
-            for k, b in tmp_batch.policy_batches.items():
-                count_dict[k] += b.count
-        multi_agent_batch = MultiAgentBatch.concat_samples(samples)
-    for pid, batch in multi_agent_batch.policy_batches.items():
-        batch.shuffle()
-        ret[pid] = batch.slice(0, sample_size)
-    assert 1 == len(set(b.count for b in ret.values())), ret
-    return ret
+# def get_cross_policy_object(multi_agent_batch, self_optimizer):
+#     """Add contents into cross_policy_object, which passed to each policy."""
+#     return None
+    # joint_batch = SampleBatch.concat_samples(
+    #     [b for (_, b) in multi_agent_batch.policy_batches.item()])
+    #
+    # def _replay(policy, replay_pid):
+    #     act, _, infos = policy.compute_actions(joint_batch['obs'])
+    #     return replay_pid, act, infos
+    #
+    # for replay_pid, act, info in \
+    #         self_optimizer.workers.local_worker().foreach_policy(_replay):
+    # if act is None:
+    #     continue
+
+    # count_dict = {k: v.count for k, v in
+    #               multi_agent_batch.policy_batches.items()}
+    # for k in self_optimizer.workers.local_worker().policy_map.keys():
+    #     if k not in count_dict:
+    #         count_dict[k] = 0
+    # sample_size = max(count_dict.values())
+    # ret = {}
+    # if min(count_dict.values()) < sample_size:
+    #     samples = [multi_agent_batch]
+    #     while min(count_dict.values()) < sample_size:
+    #         tmp_batch = self_optimizer.workers.local_worker().sample()
+    #         samples.append(tmp_batch)
+    #         for k, b in tmp_batch.policy_batches.items():
+    #             count_dict[k] += b.count
+    #     multi_agent_batch = MultiAgentBatch.concat_samples(samples)
+    # for pid, batch in multi_agent_batch.policy_batches.items():
+    #     batch.shuffle()
+    #     ret[pid] = batch.slice(0, sample_size)
+    # assert 1 == len(set(b.count for b in ret.values())), ret
+    #
+    # for pid, policy, batch in zip():
+    #     postprocess_ppo_gae(policy, batch)
+    # return ret
+    # return None
 
 
 def choose_policy_optimizer(workers, config):
@@ -219,8 +206,32 @@ def choose_policy_optimizer(workers, config):
     )
 
 
+# class ValueNetworkMixin2(object):
+#     def __init__(self, config):
+#         if config["use_gae"]:
+#             @make_tf_callable(self.get_session())
+#             def value_batch(ob, prev_action, prev_reward):
+#                 # We do not support recurrent network now.
+#                 model_out, _ = self.model({
+#                     SampleBatch.CUR_OBS: tf.convert_to_tensor(ob),
+#                     SampleBatch.PREV_ACTIONS: tf.convert_to_tensor(
+#                         prev_action),
+#                     SampleBatch.PREV_REWARDS: tf.convert_to_tensor(
+#                         prev_reward),
+#                     "is_training": tf.convert_to_tensor(False),
+#                 })
+#                 return self.model.value_function()
+#         else:
+#             @make_tf_callable(self.get_session())
+#             def value_batch(ob, prev_action, prev_reward):
+#                 return tf.zeros_like(prev_reward)
+#
+#         self._value_batch = value_batch
+
+
 def setup_mixins_modified(policy, obs_space, action_space, config):
-    AddLossMixin.__init__(policy)
+    # AddLossMixin.__init__(policy)
+    # ValueNetworkMixin2.__init__(policy, config)
     setup_mixins(policy, obs_space, action_space, config)
 
 
@@ -231,19 +242,21 @@ def validate_config_modified(config):
 CEPPOTFPolicy = PPOTFPolicy.with_updates(
     name="CEPPOTFPolicy",
     get_default_config=lambda: ceppo_default_config,
-    loss_fn=ceppo_loss,
+    # loss_fn=ceppo_loss,
     postprocess_fn=postprocess_ceppo,
-    before_loss_init=setup_mixins_modified,
-    stats_fn=kl_and_loss_stats,
-    mixins=mixin_list + [AddLossMixin]
+    # before_loss_init=setup_mixins_modified,
+    # stats_fn=kl_and_loss_stats,
+    # mixins=mixin_list + [ValueNetworkMixin2]
+    # mixins=mixin_list + [ValueNetworkMixin2]
+    # mixins=mixin_list + [ValueNetworkMixin2, AddLossMixin]
 )
 
 CEPPOTrainer = PPOTrainer.with_updates(
     name="CEPPO",
     default_config=ceppo_default_config,
     default_policy=CEPPOTFPolicy,
-    validate_config=validate_config_modified,
-    make_policy_optimizer=choose_policy_optimizer
+    # validate_config=validate_config_modified,
+    # make_policy_optimizer=choose_policy_optimizer
 )
 
 
@@ -251,13 +264,13 @@ def test_ceppo():
     from toolbox.marl.test_extra_loss import _base
 
     _base(CEPPOTrainer, False, extra_config={
-        "learn_with_peers": True,
-        "sample_batch_size": 200,
-        "train_batch_size": 400,
-        "sgd_minibatch_size": 128,
-    }, t=50000, env_name="CartPole-v0")
+        "learn_with_peers": True
+    }, env_name="CartPole-v0")
 
 
+# if __name__ == '__main__':
+#     test_ceppo()
+#
 if __name__ == '__main__':
     from ray import tune
     from toolbox import initialize_ray
@@ -271,6 +284,7 @@ if __name__ == '__main__':
     env_config = {"env_name": env_name, "agent_ids": policy_names}
     env = MultiAgentEnvWrapper(env_config)
     config = {
+        "seed": 0,
         "env": MultiAgentEnvWrapper,
         "env_config": env_config,
         "multiagent": {
@@ -280,12 +294,12 @@ if __name__ == '__main__':
             },
             "policy_mapping_fn": lambda x: x,
         },
-        "disable": True
+        # "disable": False
     }
 
     tune.run(
         CEPPOTrainer,
         name="DELETEME_TEST_CEPPO",
-        stop={"timesteps_total": 100000},
+        stop={"timesteps_total": 50000},
         config=config
     )
