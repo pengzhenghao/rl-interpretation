c66e757d448c4ccc1e6a875bcb4eb69658096d25
pengzh <pengzh@ie.cuhk.edu.hk>
2019-12-03 10:14:42 +0800

    copy codes here to correct the computing of trained / sampled steps.

--- toolbox/cooperative_exploration/cetd3.py
+++ toolbox/cooperative_exploration/cetd3.py
@@ -1,6 +1,10 @@
+import ray
 from ray.rllib.agents.ddpg.ddpg_policy import DDPGTFPolicy, SampleBatch
 from ray.rllib.agents.ddpg.td3 import TD3Trainer, TD3_DEFAULT_CONFIG
-from ray.rllib.optimizers.sync_replay_optimizer import SyncReplayOptimizer
+from ray.rllib.optimizers.sync_replay_optimizer import SyncReplayOptimizer, \
+    PrioritizedReplayBuffer, MultiAgentBatch, ray_get_and_free, \
+    pack_if_needed, \
+    DEFAULT_POLICY_ID, get_learner_stats, np
 from ray.tune.util import merge_dicts
 
 DISABLE = "disable"
@@ -28,6 +32,71 @@ class SyncReplayOptimizerWithCooperativeExploration(SyncReplayOptimizer):
                 samples.policy_batches[pid] = share_sample
             samples.count = share_sample.count
         return samples
+
+    def step(self):
+        """We correct the number of sampled steps."""
+        with self.update_weights_timer:
+            if self.workers.remote_workers():
+                weights = ray.put(self.workers.local_worker().get_weights())
+                for e in self.workers.remote_workers():
+                    e.set_weights.remote(weights)
+        with self.sample_timer:
+            if self.workers.remote_workers():
+                batch = SampleBatch.concat_samples(
+                    ray_get_and_free([
+                        e.sample.remote()
+                        for e in self.workers.remote_workers()
+                    ]))
+            else:
+                batch = self.workers.local_worker().sample()
+            # Handle everything as if multiagent
+            if isinstance(batch, SampleBatch):
+                batch = MultiAgentBatch({
+                    DEFAULT_POLICY_ID: batch
+                }, batch.count)
+            for policy_id, s in batch.policy_batches.items():
+                for row in s.rows():
+                    self.replay_buffers[policy_id].add(
+                        pack_if_needed(row["obs"]),
+                        row["actions"],
+                        row["rewards"],
+                        pack_if_needed(row["new_obs"]),
+                        row["dones"],
+                        weight=None)
+        if self.num_steps_sampled >= self.replay_starts:
+            self._optimize()
+
+        # Here!
+        # self.num_steps_sampled += batch.count
+        self.num_steps_sampled += np.mean([
+            b.count for b in batch.policy_batches.values()])
+
+    def _optimize(self):
+        """We correct the number of trained agents."""
+        samples = self._replay()
+        with self.grad_timer:
+            if self.before_learn_on_batch:
+                samples = self.before_learn_on_batch(
+                    samples,
+                    self.workers.local_worker().policy_map,
+                    self.train_batch_size)
+            info_dict = self.workers.local_worker().learn_on_batch(samples)
+            for policy_id, info in info_dict.items():
+                self.learner_stats[policy_id] = get_learner_stats(info)
+                replay_buffer = self.replay_buffers[policy_id]
+                if isinstance(replay_buffer, PrioritizedReplayBuffer):
+                    td_error = info["td_error"]
+                    new_priorities = (
+                            np.abs(td_error) + self.prioritized_replay_eps)
+                    replay_buffer.update_priorities(
+                        samples.policy_batches[policy_id]["batch_indexes"],
+                        new_priorities)
+            self.grad_timer.push_units_processed(samples.count)
+
+        # Here!
+        # self.num_steps_trained += samples.count
+        self.num_steps_trained += np.mean([
+            b.count for b in samples.policy_batches.values()])
 
 
 def make_optimizer(workers, config):
