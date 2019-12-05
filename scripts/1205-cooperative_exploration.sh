293ef6dc12e3a6968f21e9b5624ab0d647fd3f65
pengzh <pengzh@ie.cuhk.edu.hk>
2019-12-06 02:36:25 +0800

    update 1205 train script

--- scripts/1205-cooperative_exploration.sh
+++ scripts/1205-cooperative_exploration.sh
@@ -1,3 +1,3 @@
 nohup python toolbox/cooperative_exploration/train.py \
---exp-name 1205-bipedalwalker \
-> 1205-cooperative_exploration.log 2>&1 &
+--exp-name 1206-bipedalwalker \
+> 1206-cooperative_exploration.log 2>&1 &
