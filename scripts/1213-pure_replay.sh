3887c380fcc462c5dfa053d4b9ecc82d983bb096
pengzh <pengzh@ie.cuhk.edu.hk>
2019-12-13 02:24:11 +0800

    Revert "change train codes for cluster"
    This reverts commit d58d516e

--- scripts/1213-pure_replay.sh
+++ scripts/1213-pure_replay.sh
@@ -2,11 +2,7 @@
 # everything is replay, such as logp, prob, logit, instead of only replay the
 # value.
 
-expname="1213-pure_replay"
-
-srun --gres=gpu:8 -n1 --kill-on-bad-exit=1 --ntasks-per-node=1 \
-  --job-name=$expname --mem=300G \
-  python toolbox/cooperative_exploration/train.py \
-  --exp-name $expname \
-  --mode three_baselines \
-  2>&1 | tee $expname.log &
+nohup python toolbox/cooperative_exploration/train.py \
+--exp-name 1213-pure_replay \
+--mode three_baselines \
+> 1213-pure_replay.log 2>&1 &
