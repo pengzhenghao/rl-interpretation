2d820812d4065cd020e12df097b71cf00fb9be58
pengzh <pengzh@ie.cuhk.edu.hk>
2019-12-13 17:03:40 +0800

    [WIP] a bug happen, action become nan, add assertion, add nan checks

--- toolbox/cooperative_exploration/ceppo.py
+++ toolbox/cooperative_exploration/ceppo.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 from ray.rllib.agents.ppo.ppo import DEFAULT_CONFIG, validate_config, \
     update_kl
 from ray.rllib.agents.ppo.ppo_policy import postprocess_ppo_gae, \
-    make_tf_callable, setup_mixins, kl_and_loss_stats, ppo_surrogate_loss, \
+    make_tf_callable, setup_mixins, kl_and_loss_stats, \
     BEHAVIOUR_LOGITS
 from ray.tune.registry import _global_registry, ENV_CREATOR
 
@@ -455,6 +455,151 @@ def loss_ceppo(policy, model, dist_class, train_batch):
         # create policy.novelty_loss for adaptively adjust curiosity.
         novelty_loss_mse(policy, model, dist_class, train_batch)
     return ppo_surrogate_loss(policy, model, dist_class, train_batch)
+
+
+class PPOLoss(object):
+    def __init__(self,
+                 action_space,
+                 dist_class,
+                 model,
+                 value_targets,
+                 advantages,
+                 actions,
+                 prev_logits,
+                 prev_actions_logp,
+                 vf_preds,
+                 curr_action_dist,
+                 value_fn,
+                 cur_kl_coeff,
+                 valid_mask,
+                 entropy_coeff=0,
+                 clip_param=0.1,
+                 vf_clip_param=0.1,
+                 vf_loss_coeff=1.0,
+                 use_gae=True,
+                 model_config=None):
+        print("Enter PPOLoss Class")
+        tf.check_numerics(value_targets)
+        tf.check_numerics(advantages)
+        tf.check_numerics(actions)
+        tf.check_numerics(prev_logits)
+        tf.check_numerics(prev_actions_logp)
+        tf.check_numerics(vf_preds)
+        tf.check_numerics(curr_action_dist.entropy())
+        tf.check_numerics(curr_action_dist.log_std)
+        tf.check_numerics(curr_action_dist.std)
+        tf.check_numerics(value_fn)
+
+        """Constructs the loss for Proximal Policy Objective.
+
+        Arguments:
+            action_space: Environment observation space specification.
+            dist_class: action distribution class for logits.
+            value_targets (Placeholder): Placeholder for target values; used
+                for GAE.
+            actions (Placeholder): Placeholder for actions taken
+                from previous model evaluation.
+            advantages (Placeholder): Placeholder for calculated advantages
+                from previous model evaluation.
+            prev_logits (Placeholder): Placeholder for logits output from
+                previous model evaluation.
+            prev_actions_logp (Placeholder): Placeholder for prob output from
+                previous model evaluation.
+            vf_preds (Placeholder): Placeholder for value function output
+                from previous model evaluation.
+            curr_action_dist (ActionDistribution): ActionDistribution
+                of the current model.
+            value_fn (Tensor): Current value function output Tensor.
+            cur_kl_coeff (Variable): Variable holding the current PPO KL
+                coefficient.
+            valid_mask (Tensor): A bool mask of valid input elements (#2992).
+            entropy_coeff (float): Coefficient of the entropy regularizer.
+            clip_param (float): Clip parameter
+            vf_clip_param (float): Clip parameter for the value function
+            vf_loss_coeff (float): Coefficient of the value function loss
+            use_gae (bool): If true, use the Generalized Advantage Estimator.
+            model_config (dict): (Optional) model config for use in specifying
+                action distributions.
+        """
+
+        def reduce_mean_valid(t):
+            return tf.reduce_mean(tf.boolean_mask(t, valid_mask))
+
+        prev_dist = dist_class(prev_logits, model)
+
+        tf.check_numerics(prev_dist.entropy())
+        tf.check_numerics(prev_dist.log_std)
+        tf.check_numerics(prev_dist.std)
+
+        # Make loss functions.
+        logp_ratio = tf.exp(curr_action_dist.logp(actions) - prev_actions_logp)
+        action_kl = prev_dist.kl(curr_action_dist)
+        self.mean_kl = reduce_mean_valid(action_kl)
+
+        curr_entropy = curr_action_dist.entropy()
+        self.mean_entropy = reduce_mean_valid(curr_entropy)
+
+        surrogate_loss = tf.minimum(
+            advantages * logp_ratio,
+            advantages * tf.clip_by_value(logp_ratio, 1 - clip_param,
+                                          1 + clip_param))
+        self.mean_policy_loss = reduce_mean_valid(-surrogate_loss)
+
+        if use_gae:
+            vf_loss1 = tf.square(value_fn - value_targets)
+            vf_clipped = vf_preds + tf.clip_by_value(
+                value_fn - vf_preds, -vf_clip_param, vf_clip_param)
+            vf_loss2 = tf.square(vf_clipped - value_targets)
+            vf_loss = tf.maximum(vf_loss1, vf_loss2)
+            self.mean_vf_loss = reduce_mean_valid(vf_loss)
+            loss = reduce_mean_valid(
+                -surrogate_loss + cur_kl_coeff * action_kl +
+                vf_loss_coeff * vf_loss - entropy_coeff * curr_entropy)
+        else:
+            self.mean_vf_loss = tf.constant(0.0)
+            loss = reduce_mean_valid(-surrogate_loss +
+                                     cur_kl_coeff * action_kl -
+                                     entropy_coeff * curr_entropy)
+        self.loss = loss
+
+
+from ray.rllib.agents.ppo.ppo_policy import Postprocessing, ACTION_LOGP
+
+
+def ppo_surrogate_loss(policy, model, dist_class, train_batch):
+    logits, state = model.from_batch(train_batch)
+    action_dist = dist_class(logits, model)
+
+    if state:
+        max_seq_len = tf.reduce_max(train_batch["seq_lens"])
+        mask = tf.sequence_mask(train_batch["seq_lens"], max_seq_len)
+        mask = tf.reshape(mask, [-1])
+    else:
+        mask = tf.ones_like(
+            train_batch[Postprocessing.ADVANTAGES], dtype=tf.bool)
+
+    policy.loss_obj = PPOLoss(
+        policy.action_space,
+        dist_class,
+        model,
+        train_batch[Postprocessing.VALUE_TARGETS],
+        train_batch[Postprocessing.ADVANTAGES],
+        train_batch[SampleBatch.ACTIONS],
+        train_batch[BEHAVIOUR_LOGITS],
+        train_batch[ACTION_LOGP],
+        train_batch[SampleBatch.VF_PREDS],
+        action_dist,
+        model.value_function(),
+        policy.kl_coeff,
+        mask,
+        entropy_coeff=policy.entropy_coeff,
+        clip_param=policy.config["clip_param"],
+        vf_clip_param=policy.config["vf_clip_param"],
+        vf_loss_coeff=policy.config["vf_loss_coeff"],
+        use_gae=policy.config["use_gae"],
+        model_config=policy.config["model"])
+
+    return policy.loss_obj.loss
 
 
 CEPPOTFPolicy = AdaptiveExtraLossPPOTFPolicy.with_updates(
