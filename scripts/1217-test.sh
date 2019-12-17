da9281e4f5d51ce5d26624ace19f28db2739dd61
pengzh <pengzh@ie.cuhk.edu.hk>
2019-12-17 11:34:30 +0800

    add train script for 1217

--- scripts/1217-test.sh
+++ scripts/1217-test.sh
@@ -1,5 +1,5 @@
 nohup python toolbox/cooperative_exploration/train.py \
---exp-name 1216-test \
+--exp-name 1217-test \
 --mode test \
 --num-seeds 8 \
-> 1216-test.log 2>&1 &
+> 1217-test.log 2>&1 &
