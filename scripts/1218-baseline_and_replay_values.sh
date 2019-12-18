b169b2d9bf3a545665612f18f7e32efe52c2140e
pengzh <pengzh@ie.cuhk.edu.hk>
2019-12-18 23:31:11 +0800

    update train script

--- scripts/1218-baseline_and_replay_values.sh
+++ scripts/1218-baseline_and_replay_values.sh
@@ -1,15 +1,13 @@
 IP_ADDRESS=$1
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 \
 nohup python toolbox/cooperative_exploration/train.py \
 --exp-name 1218-baseline_and_replay_values \
 --mode baseline \
---num-gpus 4 --address $IP_ADDRESS \
+--address $IP_ADDRESS \
 > 1218-baseline_and_replay_values-baseline.log 2>&1 &
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 \
 nohup python toolbox/cooperative_exploration/train.py \
 --exp-name 1218-baseline_and_replay_values \
 --mode replay_values \
---num-gpus 4 --address $IP_ADDRESS \
+--address $IP_ADDRESS \
 > 1218-baseline_and_replay_values-replay_values.log 2>&1 &
