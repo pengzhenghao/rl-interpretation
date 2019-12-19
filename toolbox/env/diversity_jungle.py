16d6add8776ba0c256a47a7e62ca1161f6f8229d
pengzh <pengzh@ie.cuhk.edu.hk>
2019-12-19 16:46:39 +0800

    update env

--- toolbox/env/diversity_jungle.py
+++ toolbox/env/diversity_jungle.py
@@ -1,12 +1,10 @@
 import copy
-import matplotlib
-matplotlib.use("MacOSX")
 
 import matplotlib.pyplot as plt
 import numpy as np
-import time
 
 
+# 200 points is Solved.
 class FourWayGridWorld:
     def __init__(self, N=17, left=10, right=10, up=10, down=10):
         self.N = 17
@@ -19,10 +17,8 @@ class FourWayGridWorld:
         self.map[0, int((N - 1) / 2)] = self.up
         self.map[N - 1, int((N - 1) / 2)] = self.down
         self.map[int((N - 1) / 2), N - 1] = self.right
-        self.loc = np.array([0, 0])
-        # self.loc = np.asarray([np.random.randint(N), np.random.randint(N)])
+        self.loc = np.asarray([np.random.randint(N), np.random.randint(N)])
         self.step_num = 0
-        self.init_render = False
 
     def step(self, action):
         action = np.clip(action, -1, 1)
@@ -42,10 +38,7 @@ class FourWayGridWorld:
     def render(self):
         map_self = copy.deepcopy(self.map)
         map_self[int(self.loc[0]), int(self.loc[1])] = -5
-        if not self.init_render:
-            self.canvas = plt.imshow(map_self, animated=True)
-        self.canvas.set_data(map_self)
-        plt.draw()
+        plt.imshow(map_self)
 
     def reset(self):
         self.map = np.ones((self.N, self.N)) * (-0.1)
@@ -53,23 +46,49 @@ class FourWayGridWorld:
         self.map[0, int((self.N - 1) / 2)] = self.up
         self.map[self.N - 1, int((self.N - 1) / 2)] = self.down
         self.map[int((self.N - 1) / 2), self.N - 1] = self.right
-        # self.loc = np.asarray(
-        #     [np.random.randint(s/elf.N), np.random.randint(self.N)])
-
-        self.loc = np.array([0, 0])
+        self.loc = np.asarray(
+            [np.random.randint(self.N), np.random.randint(self.N)])
         self.step_num = 0
         return self.loc
 
 
-if __name__ == '__main__':
-    env = FourWayGridWorld()
-    obs = env.reset()
-    for t in range(100):
-        o, r, d = env.step(np.ones(2))
-        print("Current t {}, observation {}, reward {}, done {}.".format(
-            t, o, r, d)
-        )
-        env.render()
-        time.sleep(0.2)
-        if d:
-            break
+# output_i = np.zeros((17, 17))
+# output_j = np.zeros((17, 17))
+# output_i_m = np.zeros((17, 17))
+# output_j_m = np.zeros((17, 17))
+# for i in range(17):
+#     for j in range(17):
+#         # print(np.asarray([i,j]))
+#         # print(Tensor(np.asarray([i,j])))
+#         # print(state,'state')
+#         states = (Tensor(np.asarray([i, j])).float().unsqueeze(0).cuda())
+#         # print(states,'states')
+#         action_mean, action_logstd, action_w, value = network(states)
+#         '''Max'''
+#         rand = torch.argmax(action_w.squeeze())
+#         action = action_mean[rand]
+#         # print(action_mean.shape,action_w.shape)
+#         # print(action_mean,action_w)
+#         # action, logproba, _, prob_lst = network.select_action_by_ensemble(
+#         # action_mean, action_logstd,action_w)
+#         # print(action)
+#         output_i[i, j] = action[0][0]
+#         output_j[i, j] = action[0][1]
+#
+#         '''Random'''
+#         rand = torch.multinomial(action_w.squeeze(), 1)
+#         action = action_mean[rand]
+#         # print(action.shape)
+#         output_i_m[i, j] = action[0][0][0]
+#         output_j_m[i, j] = action[0][0][1]
+# plt.figure(figsize=(15, 10))
+# plt.subplot(221)
+# for i in range(17):
+#     for j in range(17):
+#         arrow(j, -i, output_j[i, j], -output_i[i, j], head_width=0.2,
+#               shape='left')
+#         # arrow(i,j,output_i[i,j],output_j[i,j],head_width=0.2,shape='left')
+# # arrow(1,1,3,3,head_width=0.2,shape='left')
+# xlim(-1, 17)
+# ylim(-17, 1)
+# yticks([2 * i - 16 for i in range(9)], [2 * i for i in
