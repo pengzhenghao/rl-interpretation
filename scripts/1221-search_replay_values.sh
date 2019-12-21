635839aac12e801fb78ae7a8ab9836a67763487f
pengzh <pengzh@ie.cuhk.edu.hk>
2019-12-21 15:01:40 +0800

    update train scheme

--- scripts/1221-search_replay_values.sh
+++ scripts/1221-search_replay_values.sh
@@ -1,4 +1,5 @@
 nohup python toolbox/cooperative_exploration/train.py \
---exp-name 1221-new_adv_1221 \
+--exp-name 1221-new_adv_1221_2 \
 --mode new_adv_1221 \
-> 1221-new_adv_1221.log 2>&1 &
+--stop 2e6 \
+> 1221-new_adv_1221_2.log 2>&1 &
