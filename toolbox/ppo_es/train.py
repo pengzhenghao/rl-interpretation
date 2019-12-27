32a09346be2fd7810020cdd78ad16adf27575260
pengzh <pengzh@ie.cuhk.edu.hk>
2019-12-27 10:20:07 +0800

    add walker config for ppo-es

--- toolbox/ppo_es/train.py
+++ toolbox/ppo_es/train.py
@@ -12,11 +12,12 @@ if __name__ == '__main__':
     parser.add_argument("--num-gpus", type=int, default=8)
     parser.add_argument("--num-seeds", type=int, default=3)
     parser.add_argument("--num-agents", type=int, default=10)
-    parser.add_argument("--env", type=str, default="Humanoid-v2")
+    # parser.add_argument("--env", type=str, default="Humanoid-v2")
     parser.add_argument("--exp-name", type=str, default="")
     # parser.add_argument("--mode", type=str, default="all")
     # parser.add_argument("--stop", type=float, default=5e6)
-    parser.add_argument("--address", type=str, default="")
+    # parser.add_argument("--address", type=str, default="")
+    parser.add_argument("--walker", action="store_true")
     args = parser.parse_args()
 
     if not args.test:
@@ -27,7 +28,7 @@ if __name__ == '__main__':
         "update_steps": tune.grid_search([100000, 200000, 500000]),
         "env": MultiAgentEnvWrapper,
         "env_config": {
-            "env_name": args.env,
+            "env_name": "Humanoid-v2",
             "num_agents": args.num_agents
         },
 
@@ -46,19 +47,43 @@ if __name__ == '__main__':
         "num_gpus": 0.8
     }
 
+    walker_config = {
+        # can change
+        "update_steps": tune.grid_search([100000, 200000, 500000]),
+        "env": MultiAgentEnvWrapper,
+        "env_config": {
+            "env_name": "Walker2d-v3",
+            "num_agents": args.num_agents
+        },
+
+        # should be fixed
+        "kl_coeff": 1.0,
+        "num_sgd_iter": 20,
+        "lr": 0.0001,
+        'sample_batch_size': 256,
+        'sgd_minibatch_size': 4096,
+        'train_batch_size': 65536,
+        "num_gpus": 0.5,
+        "num_cpus_per_worker": 0.5,
+        "num_envs_per_worker": 22,
+        'num_workers': 12,
+    }
+
     train(
-        extra_config=humanoid_config,
+        extra_config=humanoid_config if not args.walker else walker_config,
         trainer=PPOESTrainer,
         env_name=args.env,
 
         stop={
             "episode_reward_mean": 6000,
             "timesteps_total": int(2e8)
+        } if not args.walker else {
+            "timesteps_total": int(5e6)
         },
         exp_name="DELETEME-TEST" if args.test else args.exp_name,
         num_agents=args.num_agents if not args.test else 3,
         num_seeds=args.num_seeds,
         num_gpus=args.num_gpus,
         test_mode=args.test,
-        address=args.address if args.address else None
+        # address=args.address if args.address else None
     )
