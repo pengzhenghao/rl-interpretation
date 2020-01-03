7bc9f9414f60b3c5402bf405d3739dd15c6c7ce5
pengzh <pengzh@ie.cuhk.edu.hk>
2020-01-03 16:47:46 +0800

    debug

--- toolbox/env/diversity_jungle.py
+++ toolbox/env/diversity_jungle.py
@@ -3,10 +3,6 @@ import numpy as np
 from gym.spaces import Box
 
 
-def _clip(x, low, high):
-    return max(min(high, x), low)
-
-
 class FourWayGridWorld(gym.Env):
     def __init__(self, env_config=None):
         self.N = 17
@@ -18,44 +14,37 @@ class FourWayGridWorld(gym.Env):
         self.action_space = Box(-1, 1, shape=(2,))
         self.early_done = env_config.get('early_done')
         self.int_initialize = not env_config.get('not_int_initialize')
+        self.map = np.ones((self.N, self.N), dtype=np.float32) * (-0.1)
+        self.map[int((self.N - 1) / 2), 0] = self.left
+        self.map[0, int((self.N - 1) / 2)] = self.up
+        self.map[self.N - 1, int((self.N - 1) / 2)] = self.down
+        self.map[int((self.N - 1) / 2), self.N - 1] = self.right
         self.reset()
 
     @property
     def done(self):
         if self.early_done:
-            return (0 >= self.x) or (self.x >= self.N - 1) or \
-                   (self.y <= 0) or (self.y >= self.N - 1) or \
+            return (not np.all(0 <= self.loc <= self.N - 1)) or \
                    (self.step_num >= 2 * self.N)
         return self.step_num >= 2 * self.N
 
     def step(self, action):
-        x = _clip(action[0], -1, 1)
-        y = _clip(action[1], -1, 1)
-        self.x = _clip(self.x + x, 0, self.N - 1)
-        self.y = _clip(self.y + y, 0, self.N - 1)
-        reward = self.map[int(self.x), int(self.y)]
+        action = np.clip(action, -1, 1)
+        self.loc = np.clip(self.loc + action, 0, self.N - 1)
+        reward = self.map[int(self.loc[0]), int(self.loc[1])]
         self.step_num += 1
-        self.loc[0] = self.x
-        self.loc[1] = self.y
         return self.loc, reward, self.done, {}
 
     def render(self, mode=None):
         pass
 
     def reset(self):
-        self.map = np.ones((self.N, self.N), dtype=np.float32) * (-0.1)
-        self.map[int((self.N - 1) / 2), 0] = self.left
-        self.map[0, int((self.N - 1) / 2)] = self.up
-        self.map[self.N - 1, int((self.N - 1) / 2)] = self.down
-        self.map[int((self.N - 1) / 2), self.N - 1] = self.right
         if self.int_initialize:
-            self.loc = np.random.randint(0, self.N - 1, size=(2,)).astype(
+            self.loc = np.random.randint(0, self.N, size=(2,)).astype(
                 np.float32)
         else:
             self.loc = np.random.uniform(0, self.N - 1, size=(2,)).astype(
                 np.float32)
-        self.x = self.loc[0]
-        self.y = self.loc[1]
         self.step_num = 0
         return self.loc
 
