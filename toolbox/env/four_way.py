ad887f160de53d614be29a88bc0e4fbdcb6511e6
pengzh <pengzh@ie.cuhk.edu.hk>
2020-01-14 13:17:53 +0800

    accelerate four_way grid, formalize some code

--- toolbox/env/four_way.py
+++ toolbox/env/four_way.py
@@ -7,6 +7,7 @@ default_config = dict(
     up=10,
     left=10,
     right=10,
+    N=16,
     early_done=False,
     int_initialize=False,
     use_walls=False,
@@ -45,20 +46,21 @@ class Wall:
         return True
 
 
+def _clip(x, low, high):
+    """clip a scalar. This is more efficient than using numpy."""
+    return max(min(x, high), low)
+
+
 class FourWayGridWorld(gym.Env):
     def __init__(self, env_config=None):
-        self.N = 16
-        self.observation_space = Box(0, self.N, shape=(2, ))
-        self.action_space = Box(-1, 1, shape=(2, ))
-
         self.config = default_config
         if isinstance(env_config, dict):
             self.config.update(env_config)
-
+        self.N = self.config['N']
+        self.observation_space = Box(0, self.N, shape=(2,))
+        self.action_space = Box(-1, 1, shape=(2,))
         self.map = np.ones((self.N + 1, self.N + 1), dtype=np.float32)
-        self.map.fill(-0.1)
         self._fill_map()
-
         self.walls = []
         self._fill_walls()
         self.reset()
@@ -72,6 +74,7 @@ class FourWayGridWorld(gym.Env):
             self.walls.append(Wall([12, 6], [12, 10]))
 
     def _fill_map(self):
+        self.map.fill(-0.1)
         self.map[int(self.N / 2), 0] = self.config['down']
         self.map[0, int(self.N / 2)] = self.config['left']
         self.map[self.N, int(self.N / 2)] = self.config['right']
@@ -81,27 +84,34 @@ class FourWayGridWorld(gym.Env):
     @property
     def done(self):
         if self.config['early_done']:
-            return (not (0 <= self.loc[0] <= self.N - 1)) or \
-                   (not (0 <= self.loc[1] <= self.N - 1)) or \
+            return (not (0 <= self.x <= self.N - 1)) or \
+                   (not (0 <= self.y <= self.N - 1)) or \
                    (self.step_num >= 2 * self.N)
         return self.step_num >= 2 * self.N
 
     def step(self, action):
-        action = np.clip(action, -1, 1).astype(np.float32)
-        new_loc = np.clip(self.loc + action, 0, self.N)
-        if any(w.intersect(self.loc, new_loc) for w in self.walls):
+        x = _clip(self.x + action[0], max(0, self.x - 1),
+                  min(self.N, self.x + 1))
+        y = _clip(self.y + action[1], max(0, self.y - 1),
+                  min(self.N, self.y + 1))
+
+        if any(w.intersect((self.x, self.y), (x, y)) for w in self.walls):
             pass
         else:
-            self.loc = new_loc
-        reward = self.map[int(round(self.loc[0])), int(round(self.loc[1]))]
+            self.x = x
+            self.y = y
+
+        loc = np.array((self.x, self.y))
+        reward = self.map[int(round(self.x)), int(round(self.y))]
         self.step_num += 1
         if self.config['record_trajectory']:
-            self.traj.append(self.loc)
-        return self.loc, reward, self.done, {}
+            self.traj.append(loc)
+        return loc, reward, self.done, {}
 
-    def render(self, mode=None):
+    def render(self, mode=None, **plt_kwargs):
         import matplotlib.pyplot as plt
-        fig, ax = plt.subplots()
+        fig = plt.figure(**plt_kwargs)
+        ax = fig.add_subplot()
         img = ax.imshow(
             np.transpose(self.map)[::-1, :],
             aspect=1,
@@ -114,7 +124,7 @@ class FourWayGridWorld(gym.Env):
             x = [w.start[0], w.end[0]]
             y = [w.start[1], w.end[1]]
             ax.plot(x, y, c='orange')
-        if len(self.traj) > 0:
+        if len(self.traj) > 1:
             traj = np.asarray(self.traj)
             ax.plot(traj[:, 0], traj[:, 1], c='blue', alpha=0.75)
         ax.set_xlabel('X-coordinate')
@@ -127,37 +137,52 @@ class FourWayGridWorld(gym.Env):
 
     def reset(self):
         if self.config['init_loc'] is not None:
-            self.loc = np.asarray(self.config['init_loc'])
-            assert self.observation_space.contains(self.loc)
+            loc = self.set_loc(self.config['init_loc'])
         else:
             if self.config['int_initialize']:
-                self.loc = np.random.randint(
-                    0, self.N + 1, size=(2, )
+                loc = np.random.randint(
+                    0, self.N + 1, size=(2,)
                 ).astype(np.float32)
             else:
-                self.loc = np.random.uniform(
-                    0, self.N, size=(2, )
+                loc = np.random.uniform(
+                    0, self.N, size=(2,)
                 ).astype(np.float32)
+        self.x, self.y = loc[0], loc[1]
         self.step_num = 0
-        self.traj = []
-        return self.loc
+        self.traj = [loc]
+        return loc
 
     def seed(self, s=None):
         if s is not None:
             np.random.seed(s)
 
+    def set_loc(self, loc):
+        loc = np.asarray(loc)
+        assert self.observation_space.contains(loc)
+        self.x = loc[0]
+        self.y = loc[1]
+        return loc
 
-def draw(compute_action, env_config):
+
+def draw(compute_action, env_config=dict(), num_grids=32, **plt_kwargs):
     """compute_action is a function that take current obs (array with shape
     (2,)) as input and return the action: array with shape (2,)."""
     import matplotlib.pyplot as plt
     env_config['_show'] = False
     env = FourWayGridWorld(env_config)
-    fig, ax = env.render()
-    for i in range(17):
-        for j in range(17):
+    fig, ax = env.render(**plt_kwargs)
+    for i in np.linspace(0, env.N, num_grids):
+        for j in np.linspace(0, env.N, num_grids):
+            if not env.observation_space.contains(np.array([i, j])):
+                continue
             action = compute_action([i, j])
-            ax.arrow(i, j, action[0], action[1], head_width=0.2, shape='left')
+            old_loc = env.set_loc([i, j])
+            new_loc, _, _, _ = env.step(action)
+            diff = new_loc - old_loc
+            if np.linalg.norm(diff) < 1e-9:
+                continue
+            ax.arrow(old_loc[0], old_loc[1], diff[0], diff[1], head_width=0.2,
+                     shape='left')
     plt.show()
 
 
@@ -174,7 +199,7 @@ if __name__ == '__main__':
     env = FourWayGridWorld(test_env_config)
     env.loc = [8, 8]
     for i in range(1000):
-        env.step(np.random.uniform(size=(2, )) * 2 - 1)
+        env.step(np.random.uniform(size=(2,)) * 2 - 1)
     env.render()
     compute_action = lambda _: [1, 0.5]
     draw(compute_action, test_env_config)
@@ -182,25 +207,25 @@ if __name__ == '__main__':
     # test reward
     # left
     env.reset()
-    env.loc = [0, 8]
+    env.set_loc([0, 8])
     assert env.step([0, 0])[1] == 20
 
     # right
     env.reset()
-    env.loc = [16, 8]
+    env.set_loc([16, 8])
     assert env.step([0, 0])[1] == 13
 
     # down
     env.reset()
-    env.loc = [8, 0]
+    env.set_loc([8, 0])
     assert env.step([0, 0])[1] == 10
 
     # up
     env.reset()
-    env.loc = [8, 16]
+    env.set_loc([8, 16])
     assert env.step([0, 0])[1] == 5
 
     # center
     env.reset()
-    env.loc = [8, 8]
+    env.set_loc([8, 8])
     np.testing.assert_almost_equal(env.step([0, 0])[1], -0.1)
