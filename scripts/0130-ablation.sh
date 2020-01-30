053a8f31ce0facffde4b2742bcf87ab13ec8ff8f
pengzh <pengzh@ie.cuhk.edu.hk>
2020-01-30 16:25:59 +0800

    update ablation train script

---
+++ scripts/0130-ablation.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+cd ~/novel-rl/
+
+#envlist=(
+#  'Walker2DBulletEnv-v0',
+#  'HalfCheetahBulletEnv-v0',
+#  'AntBulletEnv-v0',
+#  'HopperBulletEnv-v0',
+#)
+
+modelist=(
+constrain_novelty
+use_bisector
+two_side_clip_loss
+normalize_advantage
+delay_update
+use_diversity_value_network
+only_tnb
+diversity_reward_type
+)
+
+for modename in "${modelist[@]}"; do
+  srun --gres=gpu:4 -n1 --kill-on-bad-exit=1 --ntasks-per-node=1 \
+    --job-name=$envname --mem=0 --exclusive \
+    python scripts/0130-ablation.py \
+    --exp-name=0130-ablation-$modename \
+    --mode=$modename \
+    2>&1 | tee 0130-ablation-log/$modename.log &
+done
