11aa2bc5c3e6fd53b447e327d0cc1625b507454e
pengzh <pengzh@ie.cuhk.edu.hk>
2020-02-19 11:47:35 +0800

    add clear exp_name

--- toolbox/a2v/identical_parameters.py
+++ toolbox/a2v/identical_parameters.py
@@ -244,7 +244,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     algo = args.algo
     test = args.test
-    exp_name = "{}-{}-seed{}-{}agents".format(args.exp_name, algo,
+    exp_name = "{}-{}-{}seeds-{}agents".format(args.exp_name, algo,
                                               args.init_seed, args.num_seeds)
 
     os.environ['OMP_NUM_THREADS'] = '1'
