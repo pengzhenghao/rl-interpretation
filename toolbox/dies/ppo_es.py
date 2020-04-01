31b9c8a649a8d777c57bc1564bb4b4c78fba3b92
pengzh <pengzh@ie.cuhk.edu.hk>
2020-04-02 00:40:26 +0800

    make policy building inside the config validation

--- toolbox/dies/ppo_es.py
+++ toolbox/dies/ppo_es.py
@@ -1,19 +1,39 @@
-from ray.rllib.agents.ppo.ppo import PPOTrainer, DEFAULT_CONFIG
+from ray.rllib.agents.ppo.ppo import PPOTrainer, DEFAULT_CONFIG, \
+    validate_config as original_validate
 from ray.tune.utils import merge_dicts
 
 from toolbox import initialize_ray, train
 from toolbox.dies.es_utils import run_evolution_strategies
-from toolbox.marl import get_marl_env_config, on_train_result
+from toolbox.marl import get_marl_env_config, on_train_result, \
+    MultiAgentEnvWrapper
 
 ppo_es_default_config = merge_dicts(
     DEFAULT_CONFIG,
     dict(update_steps=100000, callbacks={"on_train_result": on_train_result})
 )
 
+
+def validate_config(config):
+    tmp_env = MultiAgentEnvWrapper(config["env_config"])
+    config["multiagent"]["policies"] = {
+        "agent{}".format(i): (
+            None,
+            tmp_env.observation_space,
+            tmp_env.action_space,
+            {}
+        )
+        for i in range(num_agents)
+    }
+    config["multiagent"]["policy_mapping_fn"] = lambda x: x
+
+    original_validate(config)
+
+
 PPOESTrainer = PPOTrainer.with_updates(
     name="PPOES",
     default_config=ppo_es_default_config,
-    after_train_result=run_evolution_strategies
+    after_train_result=run_evolution_strategies,
+    validate_config=validate_config
 )
 
 if __name__ == '__main__':
--- toolbox/marl/__init__.py
+++ toolbox/marl/__init__.py
@@ -8,15 +8,4 @@ def get_marl_env_config(env_name, num_agents):
         "env_config": {"env_name": env_name, "num_agents": num_agents},
         "multiagent": {}
     }
-    tmp_env = MultiAgentEnvWrapper(config["env_config"])
-    config["multiagent"]["policies"] = {
-        "agent{}".format(i): (
-            None,
-            tmp_env.observation_space,
-            tmp_env.action_space,
-            {}
-        )
-        for i in range(num_agents)
-    }
-    config["multiagent"]["policy_mapping_fn"] = lambda x: x
     return config
