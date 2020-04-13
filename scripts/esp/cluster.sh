e595d2724678b407c3ecf4547c770e1715cbe98f
pengzh <pengzh@ie.cuhk.edu.hk>
2020-04-13 11:56:34 +0800

    add partition

--- scripts/esp/cluster.sh
+++ scripts/esp/cluster.sh
@@ -11,7 +11,8 @@ envlist=(
 )
 
 for envname in "${envlist[@]}"; do
-  srun --gres=gpu:8 -n1 --kill-on-bad-exit=1 --ntasks-per-node=1 \
+  srun -p VI_SP_Y_1080TI \
+    --gres=gpu:8 -n1 --kill-on-bad-exit=1 --ntasks-per-node=1 \
     --job-name=$algoname --mem=0 --exclusive \
     python scripts/esp/train_atari.py \
     --exp-name=esp_0413_atari \
