3d047a0c9260fe855c4d3ebc51bbe71de0c22623
pengzh <pengzh@ie.cuhk.edu.hk>
2020-04-15 09:46:27 +0800

    prepared train script for es and ppo baseline for esp

--- scripts/esp/train_baseline.py
+++ scripts/esp/train_baseline.py
@@ -1,13 +1,12 @@
 """
 This file only provide training for baseline
 """
-import shutil
-import tempfile
 import time
 
+from ray import tune
 from ray.rllib.agents.ppo.ppo import PPOTrainer, PPOTFPolicy
 
-from toolbox import initialize_ray, train
+from toolbox import train
 from toolbox.evolution import GaussianESTrainer
 from toolbox.evolution_plugin.evolution_plugin import choose_optimzier, \
     merge_dicts, DEFAULT_CONFIG
@@ -32,45 +31,73 @@ if __name__ == '__main__':
     parser = get_train_parser()
     parser.add_argument("--ppo", action="store_true")
     parser.add_argument("--es", action="store_true")
-    parser.add_argument("--es-large", action="store_true")
-    parser.add_argument("--optimizer", type=str, default="sgd")  # [adam, sgd]
+    # parser.add_argument("--optimizer", type=str, default="sgd")  # [adam, sgd]
     parser.add_argument("--stop", type=float, default=1e7)
     parser.add_argument("--local-mode", "-lm", action="store_true")
     args = parser.parse_args()
     print(args)
     local_mode = args.local_mode
     now = time.time()
-    assert int(args.ppo) + int(args.es) + int(args.es_large) == 1
+    assert int(args.ppo) + int(args.es) == 1
+
     if args.ppo:
         run = PPOSGDTrainer
         config = {
-            "env": args.env_name,
+            # config = {
+            "env": tune.grid_search([
+                'Walker2DBulletEnv-v0',
+                'HalfCheetahBulletEnv-v0',
+                'AntBulletEnv-v0',
+                'HopperBulletEnv-v0',
+            ]),
+
             "num_sgd_iter": 10,
-            "num_gpus": 1 if args.num_gpus > 0 else 0,
             "train_batch_size": 4000,
             "sample_batch_size": 200,
             "num_envs_per_worker": 8,
+            "entropy_coeff": 0.001,
+            "lambda": 0.95,
             "lr": 2.5e-4,
-            "master_optimizer_type": args.optimizer
+            "num_gpus": 0.5,  # 16 trials in one machine
+
+            # locomotion config
+            "kl_coeff": 1.0,
+
+            # SGD optimizer
+            "master_optimizer_type": tune.grid_search(["sgd", "adam"])
         }
-    if args.es or args.es_large:
+        stop = int(1e7)
+
+    if args.es:
         config = {
+            "env": tune.grid_search([
+                'Walker2DBulletEnv-v0',
+                'HalfCheetahBulletEnv-v0',
+                'AntBulletEnv-v0',
+                'HopperBulletEnv-v0',
+            ]),
+
             "train_batch_size": 4000,
             "num_workers": 10,
-            "optimizer_type": args.optimizer,
-            "env": args.env_name,
+            "optimizer_type": tune.grid_search(["sgd", "adam"]),
+            "num_gpus": 0,
             "lr": 2.5e-4,
             "episodes_per_batch": 1,
             "num_cpus_per_worker": 0.5
         }
         run = GaussianESTrainer
-        if args.es_large:
-            config.update({
-                "episodes_per_batch": 1,
-                "train_batch_size": 10000
-            })
+        stop = int(1e8)
+
+    if args.redis_password:
+        from toolbox import initialize_ray
+        import os
+
+        initialize_ray(address=os.environ["ip_head"], test_mode=args.test,
+                       redis_password=args.redis_password)
+
     train(
-        run, stop=int(args.stop), verbose=2, config=config,
+        run, stop=int(args.stop), verbose=1, config=config,
         exp_name=args.exp_name, num_seeds=args.num_seeds, num_gpus=args.num_gpus
     )
+
     print("Test finished! Cost time: ", time.time() - now)
